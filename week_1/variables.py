530953dfe73c834e71ffa99a13bd90dc672738e3
66ultimategamer <ndungumaxwell6@gmail.com>
2024-02-22 08:47:41 +0300

    variables

---
+++ week_1/variables.py
@@ -0,0 +1,19 @@
+#
+
+
+
+# integers are positive or negative whole numbers
+x= 10
+y = 24
+
+# float:in decimal / floating point
+pi= 3.142
+
+# characters : Alphanumeric ,
+# special characters ; '@ #
+grade = 'A'
+# Strings :
+
+f_name ='Maxwel'
+s_name = 'Ndungu'
+
