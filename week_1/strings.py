2beaa52dfb9c726de94f05446d2e655f74fb2397
66ultimategamer <ndungumaxwell6@gmail.com>
2024-02-22 10:15:15 +0300

    loops

---
+++ week_1/strings.py
@@ -0,0 +1,43 @@
+# string in python
+
+
+
+city= "nairobi"
+
+
+# convert to upper case
+
+print(city)
+print("\n") # uppercaase
+print(city .upper())
+
+name= "MAX"
+print(name)
+print ("\n")
+print( name.lower()) 
+
+town = "   Naivasha   "
+
+print(town)
+print("\n")
+print(town.strip())
+
+# add two strings
+
+f_name= "Maxwel"
+s_name = "ndungu"
+
+full_name= f_name + s_name
+
+print(full_name)
+
+# programme to reverse a string
+
+fruit = "orangeoooooo"
+print(fruit.replace("O","Y"))
+
+subject=("physical :sciences")
+print(subject .split(":"))
+
+age=40
+print("i am ")
---
+++ week_2/loops.py
@@ -0,0 +1,13 @@
+# 
+
+
+#for loop
+#
+
+
+for number in range (0,10):
+    print(number)
+    #squares of the number
+for x in range (0,11):
+    print (x**2)
+    print ("\t")
