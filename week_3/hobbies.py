4c51f6ed8dcf0497260df200ca41ac20ff13736d
66ultimategamer <ndungumaxwell6@gmail.com>
2024-02-29 08:25:06 +0300

    hobbies

--- week_3/hobbies.py
+++ week_3/hobbies.py
@@ -1,3 +1,3 @@
-hobbies=["gaming","watching anime","sky diving","hiking","playing football"]
+hobbies=("gaming","watching anime","sky diving","hiking","playing football")
 for hobby in hobbies:
     print(hobby)
